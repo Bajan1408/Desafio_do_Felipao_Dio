8ef5f478dba6eead74fd25c39aad805583c9602b
Bajan1408 <gianrodrigues@gmail.com>
2023-09-04 13:27:11 -0300

    updates no readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Desafio_do_Felipao_Dio
 Primeiro desafio da formação Lógica de programação da Digital Innovation One, com o instrutor Felipão.
+Usando os conceitos de variáveis, operadores e estruturas de decisão para construir um algoritmo simples.
--- heroLevel.js
+++ heroLevel.js
@@ -4,7 +4,7 @@ let heroi = "Spiderman";
 
 //Informe a quantidade de xp do herói
 
-let xp = 10200;
+let xp = 6001;
 
 let level = 0;
 
@@ -26,14 +26,14 @@ if (xp > 10000) {
     level = "Ferro";
 }
 
-console.log(`O personagem ${heroi} está no nível ${level}!`);
+console.log(`O personagem ${heroi} tem ${xp} pontos de xp está no nível ${level}!`);
 
 
 
-/*
+/* Tabela de níveis de xp
 Se XP for menor do que 1.000 = Ferro
 Se XP for entre 1.001 e 2.000 = Bronze
-Se XP for entre 2.001 e 5.000 = Prata
+Se XP for entre 2.001 e 6.000 = Prata
 Se XP for entre 6.001 e 7.000 = Ouro
 Se XP for entre 7.001 e 8.000 = Platina
 Se XP for entre 8.001 e 9.000 = Ascendente
